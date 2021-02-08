bea0d4917db804a50c1249af0f82f81ff60910f0
Jhonatan <jhonatquinteros@gmail.com>
2021-02-07 19:47:36 -0500

    finishing classic node server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { read } = require('fs');
 const http = require('http');
 
-http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{
     res.status = 200;
     res.setHeader('Content-Type','text/plain');
     res.end('Hola pendejas');
 });
 
-http.Server.listener(3000,()=>{
+erver.listen(3000,()=>{
     console.log('Server running on port 3000');
 })
